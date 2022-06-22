3f2df101c149baabe51f0b631819b3f3d36ebd09
Huergacho <75048402+Huergacho@users.noreply.github.com>
2022-06-22 20:51:39 -0300

    Random Items Instatiate

--- Assets/Scripts/Level/Room.cs
+++ Assets/Scripts/Level/Room.cs
@@ -10,10 +10,20 @@ public class Room : MonoBehaviour
 {
 
     [SerializeField]private List<Room> neighBours;
+    [SerializeField] private Dictionary<GameObject,float> objectsToInstatiate = new Dictionary<GameObject, float>();
+    [SerializeField] private List<Transform> instatiateWayPoints;
     public List<Room> NeightBours => neighBours;
 
     [SerializeField] private RoomProperties properties;
-    [SerializeField] private GameObject currentPreset;
+    //[SerializeField] private GameObject currentPreset; // Esto queda por si en algun momento queremos volver a meter lo de los presets :D
+    private void Start()
+    {
+        for (int i = 0; i < properties.InstanciableObjects.Count; i++)
+        {
+            objectsToInstatiate.Add(properties.InstanciableObjects[i].ObjectToSpawn, properties.InstanciableObjects[i].Probability);
+
+        }
+    }
     public void GetNeightBoursRadially()
     {
         Collider[] colls = Physics.OverlapSphere(transform.position, properties.DetectDistance, properties.RoomLayer);
@@ -25,20 +35,31 @@ public class Room : MonoBehaviour
             { 
               neighBours.Add(currentRoom);
             }
+        }   
+    }
+    public void InstantiateRandomEntities()
+    {
 
+        for (int i = 0; i < instatiateWayPoints.Count; i++)
+        {
+            var spawnPos = instatiateWayPoints[i];
+            var newObj = MyEngine.MyRandom.GetRandomWeight(objectsToInstatiate);
+            Instantiate(newObj, spawnPos);
+            instatiateWayPoints.RemoveAt(i);
         }
     }
-
     public void GetRandomized()
     {
-        var currRoom = UnityEngine.Random.Range(0, properties.Presets.Length);
-        if(currentPreset != null)
-        {
-            Destroy(currentPreset);
-            currentPreset = null;
-        }
-        currentPreset = properties.Presets[currRoom];
-        Instantiate(currentPreset, transform);
+        //var currRoom = UnityEngine.Random.Range(0, properties.Presets.Length);     // Esto queda por si en algun momento queremos volver a meter lo de los presets :D
+        //if(currentPreset != null)
+        //{
+        //    Destroy(currentPreset);
+        //    currentPreset = null;
+        //}
+        //currentPreset = properties.Presets[currRoom];
+        //Instantiate(currentPreset, transform);
+
+        InstantiateRandomEntities();
     }
     public void GetNeightboursLinealy()
     {
--- Assets/Scripts/Level/RoomProperties.cs
+++ Assets/Scripts/Level/RoomProperties.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-[CreateAssetMenu(fileName = "Room", menuName = "Rooms/Default",order = 0)]
+[CreateAssetMenu(fileName = "Room", menuName = "Rooms/Default", order = 0)]
 public class RoomProperties : ScriptableObject
 {
     [SerializeField] private GameObject[] _presets;
@@ -13,4 +13,17 @@ public class RoomProperties : ScriptableObject
     public LayerMask RoomLayer => _roomLayer;
     [SerializeField] private float _detectDistance;
     public float DetectDistance => _detectDistance;
+
+    [System.Serializable]
+    public class ObjectsToInstance
+    {
+        [SerializeField] private float probability;
+        public float Probability => probability;
+        [SerializeField] private GameObject objectToSpawn;
+        public GameObject ObjectToSpawn => objectToSpawn;
+
+    }
+    [SerializeField] private List<ObjectsToInstance> _instanciableObjects;
+    public List<ObjectsToInstance> InstanciableObjects => _instanciableObjects;
+
 }
